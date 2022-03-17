704407938a7af316ac7045d9200c0b5b1e9c3304
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-03-17 18:03:48 +0000

    incomplete functionality

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ let wrapper = document.querySelector(".wrapper");
 let contentDiv = document.querySelector(".main-content");
 
 
+let num = 0;
 let mainArr = [];
 let grabQuestions = (url) => {
     fetch(url)
     .then(result => result.json())
-    .then(data => mainArr.push(data))
+        .then(data => mainArr.push(data))
+        .catch((e) => {
+        console.log(e)
+    })
 }
 
 let interval; 
@@ -28,34 +32,86 @@ let valueCreator = (name) => {
 let appender = ( appendee,...items) => {
     items.map(item=> appendee.appendChild(item))
 }
+
 let createBoard = () => {
     let containerDiv = valueCreator("div");
     containerDiv.classList.add("containerDiv")
     let h2 = valueCreator("h2");
     h2.classList.add("mainH2");
-    let questionDiv = valueCreator("div");
-    let answerDiv1 = valueCreator("div");
-    let answerDiv2 = valueCreator("div");
-    let answerDiv3 = valueCreator("div");
-    let questionsArr = [h2, questionDiv, answerDiv1, answerDiv2, answerDiv3]
+    let li1 = valueCreator("li");
+    let li2 = valueCreator("li");
+    let li3 = valueCreator("li");
+    let li4 = valueCreator("li");
+    let containerUl = valueCreator("ul");
+    containerUl.classList.add("SCD")
+    let questionsArr = [h2, li1, li2, li3, li4]
     setQuestions(questionsArr)
-    appender(containerDiv, h2, questionDiv, answerDiv1, answerDiv2, answerDiv3)
+    appender(containerUl, li1, li2, li3, li4)
+    appender(containerDiv, h2, containerUl)
     appender(wrapper, containerDiv);
 }
 
-let setQuestions = (arr) => {
-    let { results } = mainArr[0];
-    let [h2Div] = arr;
-    console.log(results)
+
+let incrementer = () => {
+    return num += 1;
 }
+
+let setQuestions = (arr, reset, item) => {
+    if (reset && item ) return reset(item)  
+    let index = num;
+    let { results } = mainArr[0];
+    let [h2, li1, li2, li3, li4] = arr;
+    let answersDiv = [li1, li2, li3, li4]
+    let { question, correct_answer, incorrect_answers } = results[index];
+    h2.innerHTML = question;
+    let questionsArr = [correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5)
+    loopthroughQuestions(answersDiv, questionsArr);
+    clickFunctionforAnswers(answersDiv, correct_answer, arr)
+}
+
+const loopthroughQuestions = (divs, questions) => {
+    for (let div of divs) {
+        div.classList.add("main-li")
+        for (let question of questions) {
+            div.innerHTML = question;
+            questions.splice(questions.indexOf(question), 1)
+            break;
+        }
+    }
+
+}
+
+const clickFunctionforAnswers = (arr, correctAnswer, questionsArr) => {
+    function checkWin(item) {
+            if (item.innerText === correctAnswer) return item.classList.add("green");
+            else return item.classList.add("red");
+        
+    }
+    function reset(item) {
+        item.classList.remove("green")
+        item.classList.remove("red")
+
+    }
+    arr.map(item => {
+        item.addEventListener("click", () => { 
+            arr.forEach(item => item.classList.remove("main-li"))
+            checkWin(item)
+            incrementer();
+            setQuestions(questionsArr)
+
+        })
+    })
+}
+
+
 
 startBtn.addEventListener("click",(e)=> {
     e.preventDefault();
    interval = setInterval(checkIfArrIsEmpty, 500)
 })
 
-setTimeout(() => {
-    grabQuestions("https://opentdb.com/api.php?amount=5")
+setTimeout( () => {
+   grabQuestions("https://opentdb.com/api.php?amount=5")
 }, 2000)
 
 
